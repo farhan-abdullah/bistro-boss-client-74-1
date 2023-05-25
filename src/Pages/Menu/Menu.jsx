5b1917244f86f08965f493ec9a673af60f59baac
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-05-25 15:20:23 +0200

    25/05/2023 8:34

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -3,18 +3,27 @@ import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import img from '../../assets/menu/banner3.jpg';
 import PopulerMenu from '../Home/PopularMenu/PopulerMenu';
+import { useMenu } from '../../hooks/useMenu';
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
+import MenuCategory from './MenuCategory/MenuCategory';
 const Menu = () => {
+	const [menu] = useMenu();
+	const desserts = menu.filter((item) => item.category === 'dessert');
+	const pizzas = menu.filter((item) => item.category === 'pizza');
+	const soups = menu.filter((item) => item.category === 'soup');
+	const salads = menu.filter((item) => item.category === 'salad');
+	const offers = menu.filter((item) => item.category === 'offered');
+
 	return (
 		<div>
 			<Helmet>
 				<title> Bistro Boss | Menu</title>
 			</Helmet>
 			<Cover img={img} title={'our menu'}></Cover>
-			<PopulerMenu></PopulerMenu>
-			<Cover img={img} title={'our menu'}></Cover>
-			<PopulerMenu></PopulerMenu>
-			<Cover img={img} title={'our menu'}></Cover>
-			<PopulerMenu></PopulerMenu>
+			<SectionTitle subHeading={"Don't Miss"} heading={'Todays Offer'}></SectionTitle>
+			<MenuCategory items={offers}></MenuCategory>
+			<MenuCategory items={desserts} title={'Desert'} img={""}></MenuCategory>
+			<MenuCategory items={salads}></MenuCategory>
 		</div>
 	);
 };
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import MenuItem from '../../../Shared/MenuItem/MenuItem';
+import Cover from '../../../Shared/Cover/Cover';
 
-const MenuCategory = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+const MenuCategory = ({ items, title, img }) => {
+	return (
+		<div>
+			{title && <Cover img={img} title={title}></Cover>}
+			<div className='p-5 grid md:grid-cols-2 gap-4 items-center'>
+				{items.map((item) => (
+					<MenuItem key={item._id} item={item}></MenuItem>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default MenuCategory;
