22b11742033319ab4ed5e0ec50f03f82e67e7c6a
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-05-25 14:53:43 +0200

    75-3 Create Custom Hook Usemenu To Load Menu Data

--- src/Pages/Home/PopularMenu/PopulerMenu.jsx
+++ src/Pages/Home/PopularMenu/PopulerMenu.jsx
@@ -1,22 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import MenuItem from '../../../Shared/MenuItem/MenuItem';
+import { useMenu } from '../../../hooks/useMenu';
 
 const PopulerMenu = () => {
-	const [menu, setMenu] = useState([]);
-	useEffect(() => {
-		fetch('menu.json')
-			.then((res) => res.json())
-			.then((data) => {
-				const popularItems = data.filter((item) => item.category === 'popular');
-				setMenu(popularItems);
-			});
-	}, []);
+	// const [menu, setMenu] = useState([]);
+	const [menu] = useMenu();
+	const popularItems = menu.filter((item) => item.category === 'popular');
 	return (
 		<section>
 			<SectionTitle subHeading={'Popular Items'} heading={'FROM OUR MENU'}></SectionTitle>
 			<div className='p-5 grid md:grid-cols-2 gap-4 items-center'>
-				{menu.map((item) => (
+				{popularItems.map((item) => (
 					<MenuItem key={item._id} item={item}></MenuItem>
 				))}
 			</div>
--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-
+import Cover from '../../Shared/Cover/Cover';
+import img from '../../assets/menu/banner3.jpg';
+import PopulerMenu from '../Home/PopularMenu/PopulerMenu';
 const Menu = () => {
 	return (
 		<div>
 			<Helmet>
 				<title> Bistro Boss | Menu</title>
 			</Helmet>
+			<Cover img={img} title={'our menu'}></Cover>
+			<PopulerMenu></PopulerMenu>
+			<Cover img={img} title={'our menu'}></Cover>
+			<PopulerMenu></PopulerMenu>
+			<Cover img={img} title={'our menu'}></Cover>
+			<PopulerMenu></PopulerMenu>
 		</div>
 	);
 };
