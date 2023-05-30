13b11d3a3d0e370fee0210bc164e7b5056f30411
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-05-30 19:23:58 +0200

    76-5 Explore React Hook Form Validation, Error Handling

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm();
+	const onSubmit = (data) => console.log(data);
+
 	const handleSignup = (event) => {
 		event.preventDefault();
 		const form = event.target;
@@ -20,39 +29,55 @@ const Signup = () => {
 					</p>
 				</div>
 				<div className='card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
-					<form onSubmit={handleSignup} className='card-body'>
+					<form onSubmit={handleSubmit(onSubmit)} className='card-body'>
 						<div className='form-control'>
 							<label className='label'>
 								<span className='label-text'>Name</span>
 							</label>
 							<input
+								{...register('name', { required: true })}
 								type='text'
 								name='name'
 								placeholder='Name'
 								className='input input-bordered'
 							/>
+							{errors.name && (
+								<span className='p-2 text-orange-700'>This field is required</span>
+							)}
 						</div>
 						<div className='form-control'>
 							<label className='label'>
 								<span className='label-text'>Email</span>
 							</label>
 							<input
+								{...register('email', { required: true })}
 								type='text'
 								name='email'
 								placeholder='email'
 								className='input input-bordered'
 							/>
+							{errors.email && (
+								<span className='p-2 text-orange-700'>This field is required</span>
+							)}
 						</div>
 						<div className='form-control'>
 							<label className='label'>
 								<span className='label-text'>Password</span>
 							</label>
 							<input
+								{...register('password', {
+									required: true,
+									maxLength: 20,
+									minLength: 6,
+								})}
 								type='text'
 								name='password'
 								placeholder='password'
 								className='input input-bordered'
 							/>
+							{errors.password?.type === 'required' && (
+								<p className='p-2 text-orange-700'>Password is required</p>
+							)}
 						</div>
 						<div className='form-control mt-6'>
 							<button className='btn btn-primary'>Login</button>
