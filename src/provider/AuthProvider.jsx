7a858ae219b817ca29a0f5dc22cba250fb69068f
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-06-04 18:51:33 +0200

    jwt: riceve token and save in local storage

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -11,6 +11,7 @@ import {
 	updateProfile,
 } from 'firebase/auth';
 import app from '../firebase/firebase.config';
+import axios from 'axios';
 
 const AuthProvider = ({ children }) => {
 	const [user, setUser] = useState(null);
@@ -37,6 +38,16 @@ const AuthProvider = ({ children }) => {
 	useEffect(() => {
 		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
 			setUser(currentUser);
+			//get and set token
+			if (currentUser) {
+				axios
+					.post('http://localhost:5000/jwt', { email: currentUser.email })
+					.then((data) => {
+						localStorage.setItem('access-token', data.data.token);
+					});
+			} else {
+				localStorage.removeItem('access-token');
+			}
 			setLoading(false);
 		});
 		return () => {
